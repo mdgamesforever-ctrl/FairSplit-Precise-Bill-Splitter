7233551c99cfae360449752cd582cee01a3a1596
mdgamesforever-ctrl <mdgamesforever@gmail.com>
2026-08-12 15:25:52 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.tabsplit.app"
+    applicationId = "com.futurepath.fairsplit""
     minSdk = 24
     targetSdk = 36
     versionCode = 1
