4857bfa5c5497f69066769a813e5c0c6b8eac40b
mdgamesforever-ctrl <mdgamesforever@gmail.com>
2026-08-12 15:27:45 +0300

    Fix applicationId formatting in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.futurepath.fairsplit""
+    applicationId = "com.futurepath.fairsplit"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
