739822cce6dc7ba65e59a5a4c70f19155fced7b0
mdgamesforever-ctrl <mdgamesforever@gmail.com>
2026-08-16 10:46:05 +0300

    Increment versionCode from 1 to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     applicationId = "com.futurepath.fairsplit"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
+    versionCode = 2
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
